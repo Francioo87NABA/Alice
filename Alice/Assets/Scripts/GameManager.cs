d97cd6ffc3c7ed0a65da753f38da17599b565d6a
Francioo87NABA <francesco_palma@studenti.naba.it>
2020-11-17 19:31:23 +0100

    Unity è bello
    inizio script gamemanager
    animazione apertura chiusura porta

--- Alice/Assets/Scripts/GameManager.cs
+++ Alice/Assets/Scripts/GameManager.cs
@@ -8,6 +8,15 @@ public class GameManager : MonoBehaviour
     public static GameManager Singleton;
     
     public bool start;
+    public bool dentroPorta1;
+
+    public GameObject porta1;
+    public Transform transformPorta1;
+    private Animator animatorPorta1;
+
+    private int one;
+    private int two;
+    
 
     private void OnEnable()
     {
@@ -16,11 +25,47 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        
+        animatorPorta1 = porta1.GetComponentInChildren<Animator>();
     }
     
     void Update()
     {
+        if (one == 0 && start)
+        {
+            StartCoroutine(Sequenza1());
+            one++;
+        }
+
+        if (two == 0 && dentroPorta1)
+        {
+            StartCoroutine(Sequenza2());
+            two++;
+        }
         
+        
+            
+    }
+
+    IEnumerator Sequenza1()
+    {
+        //instanzia audio
+        
+        yield return new WaitForSeconds(5f); //DEVE ESSERE 15 SECONDI
+        
+        porta1.SetActive(true);
+        porta1.transform.position = transformPorta1.position;
+        
+        yield return new WaitForSeconds(5f);
+        
+        animatorPorta1.SetBool("Apriti",true);
+    }
+
+    IEnumerator Sequenza2()
+    {
+        //Instanzia audio
+        
+        yield return new WaitForSeconds(5f);
+        
+        animatorPorta1.SetBool("Apriti", false);
     }
 }
---
+++ Alice/Assets/Scripts/Porta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porta : MonoBehaviour
+{
+
+    public bool porta1;
+    public bool porta2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (porta1)
+            {
+                GameManager.Singleton.dentroPorta1 = true;
+            }
+            else if (porta2)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+       
+    }
+}
